c7d952ade1bd520a219df57906f7a945c101749c
한국대표서민 <123250244+no1seomin@users.noreply.github.com>
2023-01-23 23:46:37 +0900

    Update Helloworld.java

--- Helloworld.java
+++ Helloworld.java
@@ -2,7 +2,7 @@ public class HelloWorld{
 
      public static void main(String []args){
             String[] alphabet = new String[] {"a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","!",","};
-      String[] hello = "hello, world!".split("");
+      String[] hello = "hello, world!".split(""); // 스트링을 글자 하나하나 쪼개기
       
        
        for(int i = 0; i<hello.length;i++)
@@ -15,7 +15,7 @@ public class HelloWorld{
                }
                 System.out.println(alphabet[j]);
                 
-                if(alphabet[j].equals(hello[i])){
+                if(alphabet[j].equals(hello[i])){ // 글자 하나하나 비교하기
                     break;}
                     
                else continue;
